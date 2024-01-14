77088abe93fbc26f419ee5d93ace80f7aebf0142
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-01-14 14:57:22 +0530

    Broadcasted message to all active clients and UI changes

--- client.js
+++ client.js
@@ -6,12 +6,54 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    })
+  })
+}
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    })
+  })
+}
+
+
+
+
 // * socekt = net.Socket (Duplex Stream)
 const socket = net.createConnection({ host: '127.0.0.1', port: 3008 }, async () => {
-  console.log("Connected to the server");
+  console.log("Connected to the server\n");
+})
 
+
+const ask = async () => {
   const message = await rl.question('Enter a message > ');
+  // * move cursor one line up
+  await moveCursor(0, -1);
+  // * Clear the current line that the cursor is in
+  await clearLine(0);
   socket.write(message);
+}
+
+socket.on('connect', () => {
+  ask();
+})
+
+
+socket.on('data', async (data) => {
+  // * Log an empty line
+  console.log();
+  // * Move the cursor one line up
+  await moveCursor(0, -1);
+  // * Clear that line that cursor just moved into
+  await clearLine(0)
+  console.log(data.toString('utf-8'));
+  ask();
 })
 
 socket.on('close', () => {
--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const server = net.createServer();
 // * To set max connection limit to server
 // server.maxConnections = 1;
 
+const clients = [];
+
 server.on('connection', (socket) => {
   // * socekt = net.Socket (Duplex Stream)
   console.log('A new connection to the server');
-  // socket.write('Hii there');
+  clients.push(socket);
+
   socket.on('data', (chunk) => {
-    console.log(chunk.toString());
+    // console.log(chunk.toString('utf-8'));
+    clients.map(s => {
+      s.write(chunk);
+    })
   })
 })
 
